8917c0c4733bd20ab0126783ffb7e57b4dd39b97
Mu Yang <emfomy@gmail.com>
2016-10-27 21:52:12 +0800

    Update project pages.

--- include/nav.js
+++ include/nav.js
@@ -2,7 +2,9 @@ document.write("<nav id=\"nav\">");
 document.write("	<ul>");
 document.write("		<li><a href=\"index.html\">Home<\/a><\/li>");
 document.write("		<li><a href=\"member\/index.html\">Members<\/a><\/li>");
-document.write("		<li><a href=\"project\/index.html\">Projects<\/a><\/li>");
+document.write("		<li><a href=\"project\/mcnla\/index.html\">MCNLA<\/a><\/li>");
+document.write("		<li><a href=\"project\/isvd\/index.html\">iSVD<\/a><\/li>");
+document.write("		<li><a href=\"project\/pass\/index.html\">PaSS<\/a><\/li>");
 document.write("	<\/ul>");
 document.write("<\/nav>");
 
