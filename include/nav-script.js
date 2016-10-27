c09df9052603907f347f7a08fd2b711d873ea408
Mu Yang <emfomy@gmail.com>
2016-10-28 00:00:26 +0800

    Improve navigation bar.

--- include/nav-script.js
+++
@@ -1,7 +0,0 @@
-document.write("<script>");
-document.write("for (var i = 0; i < document.links.length; i++) {");
-document.write("	if (document.links[i].href == document.URL || document.links[i].href == document.URL+'index.html') {");
-document.write("		document.links[i].className = 'active';");
-document.write("	} else {document.links[0].className = 'active';}");
-document.write("}");
-document.write("<\/script>");
--- include/nav.js
+++ include/nav.js
@@ -7,4 +7,9 @@ document.write("		<li><a href=\"project\/isvd\/index.html\">iSVD<\/a><\/li>");
 document.write("		<li><a href=\"project\/pass\/index.html\">PaSS<\/a><\/li>");
 document.write("	<\/ul>");
 document.write("<\/nav>");
-
+navlist = document.getElementById("nav").getElementsByTagName("a");
+for ( var i = 0; i < navlist.length; i++ ) {
+	if (navlist[i].href == document.URL || navlist[i].href == document.URL+'index.html') {
+		navlist[i].className = 'active';
+	}
+}
