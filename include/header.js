c859b351c27a022712e5b707fda5ba5f2c9a4a98
Mu Yang <emfomy@gmail.com>
2016-10-27 22:21:50 +0800

    Change header to javascript.

---
+++ include/header.js
@@ -0,0 +1,4 @@
+document.write("<header id=\"header\">");
+document.write("	<h1><a href=\"#\">WLab<\/a><\/h1>");
+document.write("	<p>Ipsum dolor sit amet nullam<\/p>");
+document.write("<\/header>");
