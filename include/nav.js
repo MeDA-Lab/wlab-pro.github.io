6f7b7c504d36901976796dd2917f6f63c52c2f83
Mu Yang <emfomy@gmail.com>
2016-10-28 14:46:59 +0800

    Add 'Get In Touch' to navigation bar.

--- include/nav.js
+++ include/nav.js
@@ -1,20 +1,22 @@
 document.write("<nav id=\"nav\">");
 document.write("	<ul>");
 document.write("		<li class=\"navicon\">");
-document.write("			<a href=\"javascript:void(0);\" onclick=\"openNavMenu()\" class=\"icon fa-bars\"><\/a>");
+document.write("			<a href=\"javascript:void(0);\" onclick=\"openNav()\" class=\"icon fa-bars\"><\/a>");
 document.write("		<\/li>");
 document.write("		<br class=\"navicon\">");
 document.write("	<\/ul>");
 document.write("	<ul id=\"navmenu\">");
-document.write("		<li><a href=\"index.html\">Home<\/a><\/li>");
-document.write("		<li><a href=\"member\/index.html\">Members<\/a><\/li>");
-document.write("		<li><a href=\"project\/mcnla\/index.html\">MCNLA<\/a><\/li>");
-document.write("		<li><a href=\"project\/isvd\/index.html\">iSVD<\/a><\/li>");
-document.write("		<li><a href=\"project\/pass\/index.html\">PaSS<\/a><\/li>");
+document.write("		<li><a href=\"\/\">Home<\/a><\/li>");
+document.write("		<li><a href=\"\/member\/\">Members<\/a><\/li>");
+document.write("		<li><a href=\"\/project\/mcnla\/\">MCNLA<\/a><\/li>");
+document.write("		<li><a href=\"\/project\/isvd\/\">iSVD<\/a><\/li>");
+document.write("		<li><a href=\"\/project\/pass\/\">PaSS<\/a><\/li>");
+document.write("		<li><a href=\"#footer\" onclick=\"setTimeout(updateNav, 0)\">Get In Touch<\/a><\/li>");
 document.write("	<\/ul>");
 document.write("<\/nav>");
 
-function openNavMenu() {
+
+function openNav() {
 	var x = document.getElementById("navmenu");
 	if (x.className === "responsive") {
 		x.className = "";
@@ -23,11 +25,17 @@ function openNavMenu() {
 	}
 }
 
-navlist = document.getElementById("nav").getElementsByTagName("a");
-docurl = location.pathname;
-for ( var i = 1; i < navlist.length; i++ ) {
-	navurl = navlist[i].pathname;
-	if ( navurl == docurl || navurl == docurl+'index.html' ) {
-		navlist[i].className = 'active';
+function updateNav() {
+	navlist = document.getElementById("nav").getElementsByTagName("a");
+	docurl = location.pathname;
+	for ( var i = 1; i < navlist.length; i++ ) {
+		navurl = navlist[i].pathname+navlist[i].hash;
+		if ( navurl == docurl || navurl == docurl+'index.html' ) {
+			navlist[i].className = 'active';
+		} else {
+			navlist[i].className = '';
+		}
 	}
 }
+
+updateNav();
