4eaf6b5fa504e945fb68cb96173a15d75d42cad4
Mu Yang <emfomy@gmail.com>
2016-10-29 13:51:28 +0800

    Add more icons with different resolution.

--- include/head.js
+++ include/head.js
@@ -5,9 +5,19 @@ document.write("<link rel=\"stylesheet\" href=\"assets\/css\/main.css\" \/>");
 document.write("<!--[if lte IE 9]><link rel=\"stylesheet\" href=\"assets\/css\/ie9.css\" \/><![endif]-->");
 document.write("<!--[if lte IE 8]><link rel=\"stylesheet\" href=\"assets\/css\/ie8.css\" \/><![endif]-->");
 
-document.write("<link rel=\"icon\" href=\"\/images\/logo32.png\" type=\"image\/png\">");
-document.write("<link rel=\"shortcut icon\" href=\"\/images\/logo32.png\" type=\"image\/png\">");
-document.write("<link rel=\"apple-touch-icon\" href=\"\/images\/logo60.png\" type=\"image\/png\">");
-document.write("<link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"\/images\/logo76.png\" type=\"image\/png\">");;
-document.write("<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"\/images\/logo120.png\" type=\"image\/png\">");;
-document.write("<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"\/images\/logo152.png\" type=\"image\/png\">");;
+document.write("<link rel=\"apple-touch-icon\" sizes=\"192x192\" href=\"\/images\/logo192.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"\/images\/logo180.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"\/images\/logo152.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"\/images\/logo120.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" sizes=\"76x76\"   href=\"\/images\/logo76.png\"  type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\"                   href=\"\/images\/logo57.png\"  type=\"image\/png\">");
+
+document.write("<link rel=\"icon\"             sizes=\"256x256\" href=\"\/images\/logo256.png\" type=\"image\/png\">");
+// Android device High Resolution
+document.write("<link rel=\"icon\"             sizes=\"192x192\" href=\"\/images\/logo192.png\" type=\"image\/png\">");
+// Android device Normal Resolution
+document.write("<link rel=\"icon\"             sizes=\"128x128\" href=\"\/images\/logo128.png\" type=\"image\/png\">");
+
+// Normal icon
+document.write("<link rel=\"icon\"                               href=\"\/images\/logo.ico\"  type=\"image\/vnd.microsoft.icon\">");
+document.write("<link rel=\"shortcut icon\"                      href=\"\/images\/logo.ico\"  type=\"image\/vnd.microsoft.icon\">");
