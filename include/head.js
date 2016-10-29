d7649f9af57f8bf5bfaed96999300139b40387b3
Mu Yang <emfomy@gmail.com>
2016-10-29 13:37:48 +0800

    Update logo and favicon.

--- include/head.js
+++ include/head.js
@@ -5,3 +5,9 @@ document.write("<link rel=\"stylesheet\" href=\"assets\/css\/main.css\" \/>");
 document.write("<!--[if lte IE 9]><link rel=\"stylesheet\" href=\"assets\/css\/ie9.css\" \/><![endif]-->");
 document.write("<!--[if lte IE 8]><link rel=\"stylesheet\" href=\"assets\/css\/ie8.css\" \/><![endif]-->");
 
+document.write("<link rel=\"icon\" href=\"\/images\/logo32.png\" type=\"image\/png\">");
+document.write("<link rel=\"shortcut icon\" href=\"\/images\/logo32.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" href=\"\/images\/logo60.png\" type=\"image\/png\">");
+document.write("<link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"\/images\/logo76.png\" type=\"image\/png\">");;
+document.write("<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"\/images\/logo120.png\" type=\"image\/png\">");;
+document.write("<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"\/images\/logo152.png\" type=\"image\/png\">");;
