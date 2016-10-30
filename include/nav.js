47b5acb2ef89e5105872065bd89022654c643f76
Mu Yang <emfomy@gmail.com>
2016-10-31 02:27:01 +0800

    Reduce line height of navigation bar.

--- include/nav.js
+++ include/nav.js
@@ -1,29 +1,37 @@
 document.write("<nav id=\"nav\">");
-document.write("	<ul>");
-document.write("		<li class=\"navicon\">");
-document.write("			<a href=\"javascript:void(0);\" onclick=\"openNav();\"><i class=\"icon fa-bars\"><\/i><\/a>");
-document.write("		<\/li>");
-document.write("		<br class=\"navicon\">");
-document.write("	<\/ul>");
-document.write("	<ul id=\"navmenu\">");
+document.write("	<ul class=\"navibar\">");
+document.write("		<li><a href=\"javascript:void(0);\" onclick=\"openNav();\"><i class=\"icon fa-bars\"><\/i><\/a><\/li>");
 document.write("		<li><a href=\"\/\"><i class=\"icon fa-home\"><\/i><\/a><\/li>");
+document.write("		<li><a href=\"#footer\" onclick=\"closeNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-globe\"><\/i><\/a><\/li>");
+document.write("		<li><a href=\"#header\" onclick=\"closeNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-angle-double-up\"><\/i><\/a><\/li>");
+document.write("	<\/ul>");
+document.write("	<ul>");
+document.write("	<\/ul>");
+document.write("	<ul id=\"navmenu\" class=\"navmenu\">");
+document.write("	  <br><br>");
+document.write("		<li class=\"desktop\"><a href=\"\/\"><i class=\"icon fa-home\"><\/i><\/a><\/li>");
 document.write("		<li><a href=\"\/member\/\">Members<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/mcnla\/\">MCNLA<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/isvd\/\">iSVD<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/pass\/\">PaSS<\/a><\/li>");
-document.write("		<li><a href=\"#footer\" onclick=\"openNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-globe\"><\/i><\/a><\/li>");
-document.write("		<li><a href=\"#header\" onclick=\"openNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-angle-double-up\"><\/i><\/a><\/li>");
+document.write("		<li class=\"desktop\"><a href=\"#footer\" onclick=\"setTimeout(updateNav, 0);\"><i class=\"icon fa-globe\"><\/i><\/a><\/li>");
+document.write("		<li class=\"desktop\"><a href=\"#header\" onclick=\"setTimeout(updateNav, 0);\"><i class=\"icon fa-angle-double-up\"><\/i><\/a><\/li>");
 document.write("	<\/ul>");
 document.write("<\/nav>");
 
 
 function openNav() {
 	var x = document.getElementById("navmenu");
-	if (x.className === "responsive") {
-		x.className = "";
+	if (x.className === "navmenu") {
+		x.className = "navmenu responsive";
 	} else {
-		x.className = "responsive";
+		x.className = "navmenu";
 	}
+}
+
+function closeNav() {
+	var x = document.getElementById("navmenu");
+	x.className = "navmenu";
 }
 
 function updateNav() {
