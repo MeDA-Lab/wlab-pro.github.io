31eb5929744bd9b0d684ce04bf6b364a2ccc6d0b
Mu Yang <emfomy@gmail.com>
2016-10-30 13:54:16 +0800

    Update fa icons.

--- include/nav.js
+++ include/nav.js
@@ -1,17 +1,18 @@
 document.write("<nav id=\"nav\">");
 document.write("	<ul>");
 document.write("		<li class=\"navicon\">");
-document.write("			<a href=\"javascript:void(0);\" onclick=\"openNav()\" class=\"icon fa-bars\"><\/a>");
+document.write("			<a href=\"javascript:void(0);\" onclick=\"openNav();\"><i class=\"icon fa-bars\"><\/i><\/a>");
 document.write("		<\/li>");
 document.write("		<br class=\"navicon\">");
 document.write("	<\/ul>");
 document.write("	<ul id=\"navmenu\">");
-document.write("		<li><a href=\"\/\">Home<\/a><\/li>");
+document.write("		<li><a href=\"\/\"><i class=\"icon fa-home\"><\/i><\/a><\/li>");
 document.write("		<li><a href=\"\/member\/\">Members<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/mcnla\/\">MCNLA<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/isvd\/\">iSVD<\/a><\/li>");
 document.write("		<li><a href=\"\/project\/pass\/\">PaSS<\/a><\/li>");
-document.write("		<li><a href=\"#footer\" onclick=\"setTimeout(updateNav, 0)\">Get In Touch<\/a><\/li>");
+document.write("		<li><a href=\"#footer\" onclick=\"openNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-globe\"><\/i><\/a><\/li>");
+document.write("		<li><a href=\"#header\" onclick=\"openNav();setTimeout(updateNav, 0);\"><i class=\"icon fa-angle-double-up\"><\/i><\/a><\/li>");
 document.write("	<\/ul>");
 document.write("<\/nav>");
 
@@ -30,7 +31,7 @@ function updateNav() {
 	docurl = location.pathname;
 	for ( var i = 1; i < navlist.length; i++ ) {
 		navurl = navlist[i].pathname+navlist[i].hash;
-		if ( navurl == docurl || navurl == docurl+'index.html' ) {
+		if ( docurl.replace('index.html', '') == navurl.replace('index.html', '') ) {
 			navlist[i].className = 'active';
 		} else {
 			navlist[i].className = '';
