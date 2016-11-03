f1b92e10e99361ebce5665d39d5e526381dbdbb6
Mu Yang <emfomy@gmail.com>
2016-11-03 17:35:29 +0800

    Add scripts for members & alumni.

--- include/header.js
+++ include/header.js
@@ -1,5 +1,5 @@
-document.write("<header id=\"header\">");
-document.write("	<a href=\"\" class=\"logo\" style=\"border-bottom:none\"><img src=\"images\/logo.svg\" height=\"96\" width=\"96\" alt=\"\" \/><\/a>");
-document.write("	<h1>WLab<\/h1>");
-document.write("	<p>Lorem ipsum dolor sit amet.<\/p>");
-document.write("<\/header>");
+document.write('<header id="header">');
+document.write('	<a href="" class="logo" style="border-bottom:none"><img src="images/logo.svg" height="96" width="96" alt="" /></a>');
+document.write('	<h1>WLab</h1>');
+document.write('	<p>Lorem ipsum dolor sit amet.</p>');
+document.write('</header>');
