a3e336d6a99474605bb1bc5202c85fdfef3bc652
Mu Yang <emfomy@gmail.com>
2016-11-15 18:16:57 +0800

    Small updates.

--- include/header.js
+++ include/header.js
@@ -1,5 +1,5 @@
 document.write('<header id="header">');
 document.write('	<a href="" class="logo" style="border-bottom:none"><img src="images/logo.svg" height="96" width="96" /></a>');
 document.write('	<h1>WLab</h1>');
-document.write('	<p>Lorem ipsum dolor sit amet.</p>');
+document.write('	<p><b>Double Ur Life.</b></p>');
 document.write('</header>');
