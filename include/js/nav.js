a99e4e0ad3a8253c12900102965c23db759d2573
Mu Yang <emfomy@gmail.com>
2016-11-21 21:47:47 +0800

    Fix the bug of navibar displaying in iPhone Plus.

--- include/js/nav.js
+++ include/js/nav.js
@@ -1,6 +1,6 @@
 document.write('<nav id="nav">');
 document.write('	<ul>');
-document.write('		<li><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>');
+document.write('		<li class="desktop"><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>');
 document.write('		<li><a href=""><i class="icon fa-home"></i></a></li>');
 document.write('		<li class="navicon"><a href="project"><i class="icon fa-puzzle-piece"></i><span>Highlights</span></a></li>');
 document.write('		<li class="navicon"><a href="member"><i class="icon fa-users"></i><span>About Us</span></a></li>');
