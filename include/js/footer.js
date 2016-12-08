f9c6aebdded1093b53c86af1c75baa5943b313c5
Mu Yang <emfomy@gmail.com>
2016-12-08 14:35:17 +0900

    Reduce document.write.

--- include/js/footer.js
+++ include/js/footer.js
@@ -1,12 +1,14 @@
-document.write('<footer id="footer">');
-document.write('	<section>');
-document.write('		<ul class="icons">');
-document.write('			<li><a href="mailto:team@wlab.pro" class="icon fa-envelope alt" target="_blank"><span class="label">Email</span></a></li>');
-document.write('			<li><a href="https:/github.com/wlab-pro" class="icon fa-github alt" target="_blank"><span class="label">GitHub</span></a></li>');
-document.write('			<li><a href="~" class="icon fa-youtube alt" target="_blank"><span class="label">Youtube</span></a></li>');
-document.write('			<li><a href="~" class="icon fa-facebook alt" target="_blank"><span class="label">Facebook</span></a></li>');
-document.write('			<li><a href="~" class="icon fa-twitter alt" target="_blank"><span class="label">Twitter</span></a></li>');
-document.write('		</ul>');
-document.write('	</section>');
-document.write('	<p class="copyright">Copyright &copy; <a href="http:/wlab.pro">WLab</a>. All rights reserved.</p>');
-document.write('</footer>');
+document.write(
+  '<footer id="footer">\n'
++ '	<section>\n'
++ '		<ul class="icons">\n'
++ '			<li><a href="mailto:team@wlab.pro" class="icon fa-envelope alt" target="_blank"><span class="label">Email</span></a></li>\n'
++ '			<li><a href="https:/github.com/wlab-pro" class="icon fa-github alt" target="_blank"><span class="label">GitHub</span></a></li>\n'
++ '			<li><a href="~" class="icon fa-youtube alt" target="_blank"><span class="label">Youtube</span></a></li>\n'
++ '			<li><a href="~" class="icon fa-facebook alt" target="_blank"><span class="label">Facebook</span></a></li>\n'
++ '			<li><a href="~" class="icon fa-twitter alt" target="_blank"><span class="label">Twitter</span></a></li>\n'
++ '		</ul>\n'
++ '	</section>\n'
++ '	<p class="copyright">Copyright &copy; <a href="http:/wlab.pro">WLab</a>. All rights reserved.</p>\n'
++ '</footer>\n'
+);
--- include/js/nav.js
+++ include/js/nav.js
@@ -1,13 +1,15 @@
-document.write('<nav id="nav">');
-document.write('	<ul>');
-document.write('		<li class="desktop"><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>');
-document.write('		<li><a href=""><i class="icon fa-home"></i></a></li>');
-document.write('		<li class="navicon"><a href="project"><i class="icon fa-puzzle-piece"></i><span>Highlights</span></a></li>');
-document.write('		<li class="navicon"><a href="member"><i class="icon fa-users"></i><span>About Us</span></a></li>');
-document.write('		<li><a href="#footer" onclick="setTimeout(updateNav, 0);"><i class="icon fa-share-alt"></i></a></li>');
-document.write('		<li><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>');
-document.write('	</ul>');
-document.write('</nav>');
+document.write(
+  '<nav id="nav">\n'
++ '	<ul>\n'
++ '		<li class="desktop"><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>\n'
++ '		<li><a href=""><i class="icon fa-home"></i></a></li>\n'
++ '		<li class="navicon"><a href="project"><i class="icon fa-puzzle-piece"></i><span>Highlights</span></a></li>\n'
++ '		<li class="navicon"><a href="member"><i class="icon fa-users"></i><span>About Us</span></a></li>\n'
++ '		<li><a href="#footer" onclick="setTimeout(updateNav, 0);"><i class="icon fa-share-alt"></i></a></li>\n'
++ '		<li><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>\n'
++ '	</ul>\n'
++ '</nav>\n'
+);
 
 
 function updateNav() {
