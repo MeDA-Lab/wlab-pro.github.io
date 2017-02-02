215c73834d38467773d8a9e8d0542fe5383cfa3c
Mu Yang <emfomy@gmail.com>
2017-02-02 12:10:31 +0800

    Update Mu Yang’s profile.

--- include/js/nav.js
+++ include/js/nav.js
@@ -1,7 +1,7 @@
 document.write(
   '<nav id="nav">\n'
 + '	<ul>\n'
-+ '		<li class="desktop"><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>\n'
++ '		<li><a href="#header" onclick="setTimeout(updateNav, 0);"><i class="icon fa-angle-double-up"></i></a></li>\n'
 + '		<li><a href=""><i class="icon fa-home"></i></a></li>\n'
 + '		<li class="navicon"><a href="project"><i class="icon fa-puzzle-piece"></i><span>Highlights</span></a></li>\n'
 + '		<li class="navicon"><a href="member"><i class="icon fa-users"></i><span>About Us</span></a></li>\n'
