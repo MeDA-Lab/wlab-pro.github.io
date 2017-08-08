b2f4650e35b0cbd03040418d5babf4ca60f5a21a
Mu Yang <emfomy@gmail.com>
2017-08-08 14:10:57 +0800

    Shuffle!!

--- include/js/header.js
+++ include/js/header.js
@@ -2,6 +2,6 @@ document.write(
   '<header id="header">\n'
 + '	<a href="" class="logo" style="border-bottom:none"><img src="images/logo.svg" height="96" width="96" /></a>\n'
 + '	<h1>WLab</h1>\n'
-+ '	<p><b>Double Ur Life.</b></p>\n'
++ '	<p><b>Double Your Life.</b></p>\n'
 + '</header>\n'
 );
